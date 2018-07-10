38235cceb3aa46b85949a946e8b6348d05f2414b
jmilazzo <j.milazzo@local.fr>
2018-07-10 12:05:42 +0200

    Update Application.scala

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -100,8 +100,10 @@ class Application @Inject() (wsClient: WSClient, cache: CacheApi) (implicit ec: 
   }
 
   def options(path: String) = CorsAction {
-    Ok.withHeaders(ACCESS_CONTROL_ALLOW_HEADERS -> Seq(AUTHORIZATION, CONTENT_TYPE, "Target-URL").mkString(","))
-    Ok.withHeaders(ACCESS_CONTROL_ALLOW_METHODS -> "POST, GET, OPTIONS, PATCH")
+    Ok.withHeaders(
+      ACCESS_CONTROL_ALLOW_HEADERS -> Seq(AUTHORIZATION, CONTENT_TYPE, "Target-URL").mkString(","),
+      ACCESS_CONTROL_ALLOW_METHODS -> "POST, GET, OPTIONS, PATCH"
+    )
   }
 
 }
